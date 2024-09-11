c25d4a64c8b4a188db93aed77cd46cbc42e3d835
sioxty <maksymslushayev@gmail.com>
2024-09-11 21:58:20 +0300

    7

---
+++ script_SM.py
@@ -0,0 +1,8 @@
+mat = int(input("матиматика: "))
+ukr = int(input("україська мова: "))
+phy = int(input("фізика: "))
+chi = int(input("хімія: "))
+inf = int(input("інфоматика: "))
+ball = mat + ukr + phy+ chi + inf 
+ser_ball = ball / 5
+print(ser_ball,"середній балл")
